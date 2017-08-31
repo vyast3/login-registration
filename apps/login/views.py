1ea5d54bd5f0d92166108cba9c7482a2f9293215
tanvi <tanvivyas1987@gmail.com>
2017-08-30 17:29:50 -0700

    initial commit

---
+++ apps/login/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect,HttpResponse
+from .models import User
+from django.contrib import messages
+
+
+def index(request):
+    return render(request,'login/index.html')
+
+def register(request):
+    result = User.objects.validate_registration(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request,err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    #print result.id
+    messages.success(request, "Successfully registered!")
+    return redirect('/success')
+
+
+def login(request):
+    result = User.objects.validate_login(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request, err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    messages.success(request, "Successfully logged in!")
+    return redirect('/success')
+
+def success(request):
+    try:
+        request.session['user_id']
+    except KeyError:
+        return redirect('/')
+    context = {
+        'user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, 'login/success.html', context)
